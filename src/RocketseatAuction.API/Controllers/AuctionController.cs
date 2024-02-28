acc4b3c5ee96c4c0e8a970297adc733e64bead74
FelipeSutter <lipeps61@gmail.com>
2024-02-28 18:16:03 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,24 @@
+﻿// igual o import
+using Microsoft.AspNetCore.Mvc;
+
+// namespace eh tipo o pacote
+namespace RocketseatAuction.API.Controllers;
+
+// As anotacoes em csharp sao passadas entre colchetes
+// Route eh a mesma coisa que o requestMapping, o caminho do endpoint
+// [controller] eh o nome que sera inserido baseado no nome do controller. Ex. AuctionController - api/Auction
+[Route("[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase {
+
+    [HttpGet]
+    public IActionResult GetCurrentAuction() {
+        return Ok("Felipe Sutter");
+    }
+
+    [HttpGet]
+    public IActionResult Test() {
+        return Ok("Felipe Sutter");
+    }
+
+}
