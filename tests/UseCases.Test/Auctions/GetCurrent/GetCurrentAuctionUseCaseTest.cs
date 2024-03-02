417718f5a028713a54a242a1c4459dabdb5b78d5
FelipeSutter <lipeps61@gmail.com>
2024-03-02 11:56:12 -0300

    feat: melhorando o teste unitário de auction

--- tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
+++ tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -1,7 +1,9 @@
-﻿using FluentAssertions;
+﻿using Bogus;
+using FluentAssertions;
 using Moq;
 using RocketseatAuction.API.Contracts;
 using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Enums;
 using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 namespace UseCases.Test.Auctions.GetCurrent;
@@ -11,8 +13,26 @@ public class GetCurrentAuctionUseCaseTest {
     public void Success() {
 
         // Arrange
+
+        var entity = new Faker<Auction>()
+            .RuleFor(auction => auction.Id, f => f.Random.Number(1, 1500)) // cria uma regra para o id, para gerar numeros aleatorios
+            .RuleFor(auction => auction.Name, f => f.Lorem.Word()) // gera palavra aleatoria
+            .RuleFor(auction => auction.Starts, f => f.Date.Past()) // data passada
+            .RuleFor(auction => auction.Ends, f => f.Date.Future()) // data futura
+            .RuleFor(auction => auction.Items, (f, auction) => new List<Item> { // gera uma nova lista de itens
+                new Item { 
+                    Id = f.Random.Number(1, 1500),
+                    Name = f.Commerce.ProductName(), 
+                    Brand = f.Commerce.Department(),
+                    BasePrice = f.Random.Decimal(30, 1000),
+                    Condition = f.PickRandom<Condition>(), // pega uma condition randomica do enum criado
+                    AuctionId = auction.Id, // pega o id criado do auction
+                    
+                }
+            }).Generate(); 
+        
         var mock = new Mock<IAuctionRepository>();
-        mock.Setup(i => i.GetCurrent()).Returns(new Auction());
+        mock.Setup(i => i.GetCurrent()).Returns(entity);
         
         var useCase = new GetCurrentAuctionUseCase(mock.Object);
 
@@ -22,6 +42,8 @@ public class GetCurrentAuctionUseCaseTest {
         // Assert
 
         auction.Should().NotBeNull();
+        auction.Id.Should().Be(entity.Id);
+        auction.Name.Should().Be(entity.Name);
 
     }
 
