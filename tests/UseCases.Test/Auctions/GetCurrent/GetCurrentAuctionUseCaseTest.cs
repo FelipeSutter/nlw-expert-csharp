23171035e417ec9be0aa608678666dd6f456767f
FelipeSutter <lipeps61@gmail.com>
2024-03-02 11:15:26 -0300

    feat: criação do projeto de testes e o primeiro teste de unidade

---
+++ tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Moq;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+namespace UseCases.Test.Auctions.GetCurrent;
+public class GetCurrentAuctionUseCaseTest {
+
+    [Fact] // anotação que diz que é um teste
+    public void Success() {
+
+        // Arrange
+        var mock = new Mock<IAuctionRepository>();
+        mock.Setup(i => i.GetCurrent()).Returns(new Auction());
+        
+        var useCase = new GetCurrentAuctionUseCase(mock.Object);
+
+        // Act
+        var auction = useCase.Execute();
+
+        // Assert
+
+        auction.Should().NotBeNull();
+
+    }
+
+}
